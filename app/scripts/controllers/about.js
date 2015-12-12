ee5038ef083d398dda3096b7850264108360086a
kjussila <kjussila@gmail.com>
2015-12-12 12:44:43 +0200

    Adding files

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name kotisivutApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the kotisivutApp
+ */
+angular.module('kotisivutApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
